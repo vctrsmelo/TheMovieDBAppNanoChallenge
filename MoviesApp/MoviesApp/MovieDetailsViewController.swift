cab1e1f3fb0d242778441ed7aeba6df9a92adf21
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-01 18:04:23 -0300

    Movie details screen begin

---
+++ MoviesApp/MoviesApp/MovieDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDetailsViewController.swift
+//  MoviesApp
+//
+//  Created by Marcelo Andrighetto Foltz on 01/06/17.
+//  Copyright © 2017 Marcelo Andrighetto Foltz. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var movieYear: UILabel!
+    @IBOutlet weak var movieTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
