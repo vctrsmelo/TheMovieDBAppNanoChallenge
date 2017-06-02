ed313f98950ef9fa418b541a9f12b5a30a8b3e55
Victor S Melo <victor.contas@outlook.com>
2017-06-02 17:01:00 -0300

    API access working

--- MoviesApp/MoviesApp/TmdbAPIAccess.swift
+++ MoviesApp/MoviesApp/TmdbAPIAccess.swift
@@ -6,15 +6,6 @@
 //  Copyright © 2017 Victor S Melo. All rights reserved.
 //
 
-// TODO: delete this Movie class
-class Movie{
-    
-    init(title: String, posterImage: UIImage) {
-        
-    }
-    
-}
-
 import UIKit
 class TmdbAPIAccess: NSObject {
     
@@ -29,7 +20,10 @@ class TmdbAPIAccess: NSObject {
     }
     
     /*
-     page: indicates the page of movies. Page 1 are the most recent movies.
+     countryCode: acquired through the following code
+     if let countryCode = (Locale.current as NSLocale).object(forKey: .countryCode) as? String {
+     
+     }
      */
     public static func getNowPlayingMovies(countryCode: String, succeeded: @escaping ([Movie]) -> Void){
     
@@ -52,11 +46,11 @@ class TmdbAPIAccess: NSObject {
 
                         if let movies = dic["results"] as? [[String:AnyObject]]{
 
-                            for movie in movies{
-
-                                if let movieDic = movie as? [String:AnyObject]{
+                            for movieDic in movies{
+                                
+                                if let movie = getMovieFrom(movieDictionary: movieDic){
                                     
-                                    moviesArray.append(getMovieFrom(movieDictionary: movieDic))
+                                    moviesArray.append(movie)
                                     
                                 }
                                 
@@ -101,12 +95,79 @@ class TmdbAPIAccess: NSObject {
                         
                         if let movies = dic["results"] as? [[String:AnyObject]]{
                             
-                            for movie in movies{
+                            for movieDic in movies{
 
-                                if let movieDic = movie as? [String:AnyObject]{
+                                if let movie = getMovieFrom(movieDictionary: movieDic){
                                 
-                                    moviesArray.append(getMovieFrom(movieDictionary: movieDic))
+                                    moviesArray.append(movie)
                                 
+                                }
+                            
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    succeeded(moviesArray)
+                    
+                }catch let error{
+                    
+                    print(error)
+                    
+                }
+                
+                }.resume()
+            
+        }
+        
+    }
+    
+    public static func getMoviesBy(title: String, succeeded: @escaping ([Movie]) -> Void){
+        
+        var moviesArray : [Movie] = []
+        
+        //URL for the page 1 (most recent movies)
+        print("language: \(Locale.preferredLanguages[0])")
+        if let url = URL(string: "https://api.themoviedb.org/3/search/movie?api_key="+apiKey+"&language="+Locale.preferredLanguages[0]+"&query=+"+title) {
+            
+            let request = URLRequest(url: url)
+            
+            let session = URLSession.shared
+            
+            session.dataTask(with: request) { (data, response, error) in
+                
+                
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                    
+                    if let dic = json as? [String:AnyObject]{
+                        
+                        if let movies = dic["results"] as? [[String:AnyObject]]{
+                            
+                            for movieDic in movies{
+                                
+                                if let movie = getMovieFrom(movieDictionary: movieDic){
+                                    
+                                    //if parameter title is part of the movie title
+                                    if let movieTitle = movie.title{
+                                        
+                                        if movieTitle.contains(title){
+                                    
+                                            moviesArray.append(movie)
+                                        
+                                        }
+                                    
+                                    }else if let movieOriginalTitle = movie.originalTitle{
+                                        
+                                        if movieOriginalTitle.contains(title){
+                                            
+                                            moviesArray.append(movie)
+                                            
+                                        }
+                                        
+                                    }
+                                    
                                 }
                                 
                             }
@@ -126,34 +187,14 @@ class TmdbAPIAccess: NSObject {
                 }.resume()
             
         }
+
         
     }
     
-    private static func getMovieFrom(movieDictionary movie: [String:AnyObject]) -> Movie{
-    
-        let title = movie["title"]
-        let releaseDateString : String = movie["release_date"] as! String
-        let posterUrl = imageStartingUrl+(movie["poster_path"] as! String)
-        let overview = movie["overview"]
-        let runtime = movie["runtime"]
+    public static func getMovieBy(id: CUnsignedLong, completion: @escaping (_ movie : Movie?) -> Void){
         
-        // TODO: get genres
-    
-        return nil
-    
-    }
-    
-    public static func getMovieBy(title: String, succeeded: @escaping ([Movie]) -> Void){
-        
-        
-        
-    }
-    
-    public static func getMovieBy(id: CUnsignedLong, succeeded: @escaping ([Movie]) -> Void){
-        
-        if let url = URL(string:"http://api.themoviedb.org/3/movie/157336?api_key="+(id as? String)){
-            
-            
+        if let url = URL(string:"http://api.themoviedb.org/3/movie/"+String(id)+"?api_key="+apiKey+"&language="+Locale.preferredLanguages[0]){
+
             let request = URLRequest(url: url)
             let session = URLSession.shared
             
@@ -162,13 +203,12 @@ class TmdbAPIAccess: NSObject {
                 do{
                     let json = try JSONSerialization.jsonObject(with: data!, options: [])
                     
-                    if let movie = json as? [String:AnyObject]{
-                
-                        moviesArray.append(getMovieFrom(movieDictionary: movie))
+                    if let movieDic = json as? [String:AnyObject]{
+
+                        completion(getMovieFrom(movieDictionary: movieDic))
                         
                     }
                     
-                    succeeded(moviesArray)
                     
                 } catch let error{
                     
@@ -183,5 +223,65 @@ class TmdbAPIAccess: NSObject {
     }
     
     
+    private static func getMovieFrom(movieDictionary movie: [String:AnyObject]) -> Movie?{
+        
+        var posterUrl: URL? = nil
+        if let posterPath = movie["poster_path"] as? String{
+            
+            posterUrl = URL(string: imageStartingUrl+posterPath)
+            
+        }
+        let id = movie["id"]
+        let title = movie["title"]
+        let originalTitle = movie["original_title"]
+        let releaseDate = movie["release_date"]
+        let overview = movie["overview"]
+        let runtime = movie["runtime"]
+        
+        //get genres
+        var genres : [String] = []
+        if let genresJson = movie["genres"] as? [[String:AnyObject]]{
+            
+            for currentGenreJson in genresJson{
+                
+                if let genreName = currentGenreJson["name"] as? String{
+                    
+                    genres.append(genreName)
+                    
+                }
+                
+            }
+            
+        }
+        
+        let movie = Movie(id: "\(String(describing: id))", title: title as? String, originalTitle: originalTitle as? String, genres: genres, runtime: runtime as? Int, releaseDateString: releaseDate as? String, overview: overview as? String)
+        
+        if let url = posterUrl{
+            
+            TmdbAPIAccess.getImageFromUrl(url: url){(imageData, response, error) in
+                
+                if let data = imageData, let posterImage = UIImage(data:data){
+                    
+                    movie.set(poster: posterImage)
+                    
+                }
+            }
+            
+        }
+        
+        return movie
+        
+    }
+    
+    
+    public static func getImageFromUrl(url: URL, completion: @escaping (_ imageData: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+
+        URLSession.shared.dataTask(with: url) {
+            (imageData, response, error) in
+            completion(imageData, response, error)
+        
+        }.resume()
+        
+    }
     
 }
