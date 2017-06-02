6a79e471569bf5e044a727d51fe35d8f67cfee6b
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-06-01 21:22:31 -0300

    Made the skeleton of the functions

--- MoviesApp/MoviesApp/MovieDAO.swift
+++ MoviesApp/MoviesApp/MovieDAO.swift
@@ -10,5 +10,32 @@ import Foundation
 import RealmSwift
 
 class MovieDAO : Object {
+	static private let instance = MovieDAO()
+	dynamic var id = ""
+	dynamic var title = ""
+	dynamic var releaseYear = 0
+	dynamic var genre = ""
 	
+	override static func ignoredProperties() -> [String] {
+		return ["instance"]
+	}
+	
+	// MARK: Private Methods
+	private func intoMovie() -> Any {
+		// TODO: Everything
+		return self
+	}
+	
+	// MARK: Public Methods
+	static func getMovie(id : String) -> Any? {
+		let movieDAO = RealmsConfig.getTemporaryRealm().objects(MovieDAO.self).filter("id == \(id)").first
+		
+		return movieDAO?.intoMovie()
+	}
+	
+	static func getMovies() {
+		let movies = RealmsConfig.getTemporaryRealm().objects(MovieDAO.self)
+		
+		//return movies
+	}
 }
