f43f27e509f74b2f02847ad9f55bc4edfce01a37
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-02 17:25:24 -0300

    Movie Details Screen with all movie information

--- MoviesApp/MoviesApp/MovieDetailsViewController.swift
+++ MoviesApp/MoviesApp/MovieDetailsViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
     
+    @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var movieYear: UILabel!
     @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieGenre: UILabel!
+    @IBOutlet weak var movieTime: UILabel!
+    @IBOutlet weak var movieOverview: UITextView!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var watchlistButton: UIButton!
+    @IBOutlet weak var rating1Button: UIButton!
+    @IBOutlet weak var rating2Button: UIButton!
+    @IBOutlet weak var rating3Button: UIButton!
+    @IBOutlet weak var rating4Button: UIButton!
+    @IBOutlet weak var rating5Button: UIButton!
+    @IBOutlet weak var viewPhotoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
