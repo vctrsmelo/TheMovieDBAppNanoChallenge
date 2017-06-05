89ec30b3230686d6104d966f57b3ce2dacc64fb2
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2017-06-05 18:07:59 -0300

    The database deletes itself if it's different from the models

--- MoviesApp/MoviesApp/RealmsConfig.swift
+++ MoviesApp/MoviesApp/RealmsConfig.swift
@@ -7,18 +7,65 @@
 //
 
 import Foundation
+import os.log
 import RealmSwift
 
 class RealmsConfig {
 	// MARK: Private Properties
+	private static let inMemoryID = "InMemoryRealm"
+	
 	static private let instance = RealmsConfig()
-	private let defaultRealm : Realm
-	private let temporaryRealm : Realm
+	
+	private var defaultRealm : Realm!
+	private var temporaryRealm : Realm!
 	
 	// MARK: Initializer
 	private init(){
-		defaultRealm = try! Realm()
-		temporaryRealm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "InMemoryMovie"))
+		do {
+			
+			try self.getRealms()
+			
+		} catch let error as NSError {
+			os_log("Error opening Realm", log: OSLog.default, type: .error)
+			
+			if error.code == 10 {
+				// TODO: Make migration before releasing
+				os_log("Migration required, instead, deleting database to be restarted", log: OSLog.default, type: .debug)
+				
+				let realmURL = Realm.Configuration.defaultConfiguration.fileURL!
+				let realmURLs = [
+					realmURL,
+					realmURL.appendingPathExtension("lock"),
+					realmURL.appendingPathExtension("note"),
+					realmURL.appendingPathExtension("management")
+				]
+				
+				for URL in realmURLs {
+					do {
+						try FileManager.default.removeItem(at: URL)
+					} catch {
+						os_log("Error deleting database, try doing manually", log: OSLog.default, type: .debug)
+					}
+				}
+				do {
+					try self.getRealms()
+				} catch {
+					os_log("Error opening Realm again", log: OSLog.default, type: .error)
+					fatalError()
+				}
+				
+			} else {
+				os_log("Unidentified error", log: OSLog.default, type: .error)
+				fatalError()
+			}
+			
+		}
+	}
+	
+	// MARK: Private Methods
+	private func getRealms() throws {
+		self.defaultRealm = try Realm()
+		self.temporaryRealm = try Realm(configuration: Realm.Configuration(inMemoryIdentifier: RealmsConfig.inMemoryID))
 	}
 	
 	// MARK: Public Methods
