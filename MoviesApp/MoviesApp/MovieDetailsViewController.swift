fcff03df99a8e0529ca27c7436dce5194102015e
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-05 16:54:38 -0300

    Details screen remoduled as table view

--- MoviesApp/MoviesApp/MovieDetailsViewController.swift
+++ MoviesApp/MoviesApp/MovieDetailsViewController.swift
@@ -8,29 +8,45 @@
 
 import UIKit
 
-class MovieDetailsViewController: UIViewController {
-    
-    @IBOutlet weak var movieImage: UIImageView!
-    @IBOutlet weak var movieYear: UILabel!
-    @IBOutlet weak var movieTitle: UILabel!
-    @IBOutlet weak var movieGenre: UILabel!
-    @IBOutlet weak var movieTime: UILabel!
-    @IBOutlet weak var movieOverview: UITextView!
-    
-    @IBOutlet weak var favoriteButton: UIButton!
-    @IBOutlet weak var watchlistButton: UIButton!
-    @IBOutlet weak var rating1Button: UIButton!
-    @IBOutlet weak var rating2Button: UIButton!
-    @IBOutlet weak var rating3Button: UIButton!
-    @IBOutlet weak var rating4Button: UIButton!
-    @IBOutlet weak var rating5Button: UIButton!
-    @IBOutlet weak var viewPhotoButton: UIButton!
+class MovieDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var contentTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        contentTableView.dataSource = self
+        contentTableView.delegate = self
+        
+        contentTableView.backgroundColor = UIColor.clear
+        contentTableView.layer.shadowColor = UIColor.darkGray.cgColor
+        contentTableView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        contentTableView.layer.shadowOpacity = 0.95
+        contentTableView.layer.shadowRadius = 6.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if (tableView.cellForRow(at: indexPath) as? MainInformationsTableViewCell) != nil {
+            return 237
+        } else if let currentCell = tableView.cellForRow(at: indexPath) as? MainInformationsTableViewCell {
+            return 72 + currentCell
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //if indexPath == 0 {
+            let cell = contentTableView.dequeueReusableCell(withIdentifier: "mainInformationsCell") as! MainInformationsTableViewCell
+        //}
+        
+        
+        return cell
     }
 }
