84edf4d31cfcc7fac02228f764a46d7ba9f86a9f
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-06 18:14:57 -0300

    Casting working 100%

---
+++ MoviesApp/MoviesApp/CastingCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CastingCollectionViewCell.swift
+//  MoviesApp
+//
+//  Created by Marcelo Andrighetto Foltz on 06/06/17.
+//  Copyright © 2017 Marcelo Andrighetto Foltz. All rights reserved.
+//
+
+import UIKit
+
+class CastingCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var actorPhoto: UIImageView!
+    @IBOutlet weak var actorName: UILabel!
+    @IBOutlet weak var characterName: UILabel!
+}
--- MoviesApp/MoviesApp/CastingTableViewCell.swift
+++ MoviesApp/MoviesApp/CastingTableViewCell.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class CastingTableViewCell: UITableViewCell {
+class CastingTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let cast: [Actor] = [Actor(actorName: "Kakaroto", characterName: "Goku", photo: UIImage(named: "images")),
+                         Actor(actorName: "Vegeta", characterName: "Sayajin King", photo: UIImage(named: "images")),
+                         Actor(actorName: "Gohan", characterName: "Matou Cell", photo: UIImage(named: "images"))]
+    
+    @IBOutlet weak var castingCollectionView: UICollectionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        castingCollectionView.dataSource = self
+        castingCollectionView.delegate = self
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cast.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = castingCollectionView.dequeueReusableCell(withReuseIdentifier: "castingCell", for: indexPath) as! CastingCollectionViewCell
+        cell.actorName.text = cast[indexPath.item].actorName?.uppercased()
+        cell.characterName.text = cast[indexPath.item].characterName
+        cell.actorPhoto.image = cast[indexPath.item].photo
+        return cell
     }
 }
