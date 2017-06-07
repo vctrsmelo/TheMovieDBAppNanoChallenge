c32145f505de1b975574653f35bac2b3355b1e01
Max Zorzetti <maxzorzetti@live.com>
2017-06-07 17:24:28 -0300

    Changed status bar color to white
    Added an extension to UIApplication to do so.
    Since we have a status bar view now, it might be really easy to add a blur effect later on.

--- MoviesApp/MoviesApp/AppDelegate.swift
+++ MoviesApp/MoviesApp/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+		
+		UIApplication.shared.statusBarView?.backgroundColor = .white
+		
         return true
     }
 
@@ -40,7 +43,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+extension UIApplication {
+	var statusBarView: UIView? {
+		return value(forKey: "statusBar") as? UIView
+	}
+}
