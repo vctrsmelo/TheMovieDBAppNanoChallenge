f9a5567d08c6bfc4e08ee6c151d25b406176f8d2
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-07 18:38:28 -0300

    Button animations done

--- MoviesApp/MoviesApp/MainInformationsTableViewCell.swift
+++ MoviesApp/MoviesApp/MainInformationsTableViewCell.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class MainInformationsTableViewCell: UITableViewCell {
+    
+    var isFavorite: Bool! = false
+    var isWatchlist: Bool! = false
+    var isWatched: Bool! = false
 
     @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var movieYear: UILabel!
     @IBOutlet weak var movieTitle: UILabel!
     @IBOutlet weak var movieGenre: UILabel!
@@ -23,6 +28,7 @@ class MainInformationsTableViewCell: UITableViewCell {
     @IBOutlet weak var rating3Button: UIButton!
     @IBOutlet weak var rating4Button: UIButton!
     @IBOutlet weak var rating5Button: UIButton!
+    @IBOutlet weak var watchedButton: UIButton!
     @IBOutlet weak var viewPhotoButton: UIButton!
     
     override func awakeFromNib() {
@@ -31,6 +37,26 @@ class MainInformationsTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    @IBAction func favoritePressed(_ sender: UIButton) {
+        isFavorite = !isFavorite
+        
+        if isFavorite {
+            favoriteButton.setImage(UIImage(named: "ic_heart_select"), for: UIControlState.normal)
+        } else {
+            favoriteButton.setImage(UIImage(named: "ic_heart"), for: UIControlState.normal)
+        }
+    }
+    
+    @IBAction func watchlistPressed(_ sender: UIButton) {
+        isWatchlist = !isWatchlist
+        
+        if isWatchlist {
+            watchlistButton.setImage(UIImage(named: "ic_saved_select"), for: UIControlState.normal)
+        } else {
+            watchlistButton.setImage(UIImage(named: "ic_saved"), for: UIControlState.normal)
+        }
     }
     
     @IBAction func oneStarPressed(_ sender: UIButton) {
@@ -71,5 +97,21 @@ class MainInformationsTableViewCell: UITableViewCell {
         rating3Button.setImage(UIImage(named: "ic_star-1"), for: UIControlState.normal)
         rating4Button.setImage(UIImage(named: "ic_star-1"), for: UIControlState.normal)
         rating5Button.setImage(UIImage(named: "ic_star-1"), for: UIControlState.normal)
+    }
+    
+    @IBAction func watchedPressed(_ sender: UIButton) {
+        isWatched = !isWatched
+        
+        if isWatched {
+            watchedButton.setTitleColor(yearLabel.textColor, for: UIControlState.normal)
+            watchedButton.borderColor = yearLabel.textColor
+        } else {
+            watchedButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+            watchedButton.borderColor = UIColor.white
+        }
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIButton) {
+        
     }
 }
